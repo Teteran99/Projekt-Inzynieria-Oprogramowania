c7eba6bb24fc58572295b013634bd7da3f4a5131
Mateusz Szarata <Mateusz Szarata>
2016-05-23 15:22:56 +0200

    Parsowanko

---
+++ ical.php
@@ -0,0 +1,16 @@
+﻿<!DOCTYPE HTML>
+<html lang="pl">
+	<head>
+	
+	 <meta charset="utf-8">
+	 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	 <title>Projekt</title>
+	 <link rel="stylesheet" href="style.css" type="text/css" />
+	 
+	</head>
+	
+  <body>
+
+  <h1 align = "center">Tu będzie iCal</h1>
+  </body>
+</html>
